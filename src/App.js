ec72e1e338ca2659b7d3018bde3bd79e98f905be
Manojkc15 <manojkc9845@gmail.com>
2021-12-25 17:41:36 +0530

    adds Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Card />
-    </div>
-  );
+    return (
+        <div>
+            {/* <h2>Let's get started!</h2> */}
+            <Navbar />
+            <Card />
+        </div>
+    );
 }
 
 export default App;
