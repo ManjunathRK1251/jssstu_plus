ce04c20d558f4b2bbda6b8040d43685170b69da0
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2021-12-26 20:58:45 +0530

    Position components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,19 +6,21 @@ import navbar_img from "../Images/navbar_bg.jpg"
 export default function Navbar() {
     return (
         // <div className="background-image">
-        <div className="navbar">
-
-            <div className="flex-row">
-                <a href="#About">About</a>
-                <a href="#Academic">Academic</a>
-                <a href="#Placement">Placement</a>
-                <a href="#Life-at-JSSSTU">Life at JSSSTU</a>
-                <a href="#Others">Others</a>
+        <div>
+            <div className="Navbar-bg">
             </div>
-            {/* <div className="Navbar-bg">
+            <div className="navbar">
 
-            </div> */}
+                <div className="flex-row">
+                    <a href="#About">About</a>
+                    <a href="#Academic">Academic</a>
+                    <a href="#Placement">Placement</a>
+                    <a href="#Life-at-JSSSTU">Life at JSSSTU</a>
+                    <a href="#Others">Others</a>
+                </div>
 
+
+            </div>
         </div>
         // </div>
     );
