a7c10b41a74c5951b65259537ae78ff2ee4a7dfa
Manjunath R K <rkmanjunath10000@gmail.com>
2021-12-26 10:18:26 +0530

    Add JSSSTU logo text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Card from "./Components/Card";
+import CarouselSlider from "./Components/Carousel_Slider/Carousel_slider";
+import ImageSlider from "./Components/ImageSlider";
+import Jssstu from "./Components/JSSSTU_logo/jssstu";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Card />
+      {/* <ImageSlider /> */}
+      {/* <CarouselSlider /> */}
+      <Jssstu></Jssstu>
     </div>
   );
 }
---
+++ src/Components/Carousel_Slider/Item.js
@@ -0,0 +1,10 @@
+import "./Item.css";
+import img1 from "C:/Users/Manju/Desktop/JSSSTU+/jssstu_plus/src/Images/navbar_bg.jpg";
+
+export default function Item() {
+  return (
+    <div class="Item">
+      <img src={img1} alt="" />
+    </div>
+  );
+}
