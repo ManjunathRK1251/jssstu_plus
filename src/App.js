cdbe6ca0e9ff342752ea3647e8f34e763aab15ca
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2021-12-28 00:10:37 +0530

    few modifications

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import Navbar from "./Components/Navbar";
 import Description from "./Components/Description";
 import Jssstu from "./Components/JSSSTU_logo/jssstu";
 import LoginPage from "./LoginPage/loginpage";
+import CarouselSlider from "./Components/Carousel_Slider/Carousel_slider";
 // import navbar_img from "./Images/navbar_bg.jpg"
 
 function App() {
-    return (
-        <div className="home-page">
-            {/* <h2>Let's get started!</h2> */}
-            {/* <img src={navbar_img} alt="" /> */}
-            <Navbar />
-            <Jssstu />
-            <Description />
-            {/* <LoginPage></LoginPage> */}
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <CarouselSlider></CarouselSlider>
+      {/* <h2>Let's get started!</h2> */}
+      {/* <img src={navbar_img} alt="" /> */}
+      {/* <Navbar />
+      <Jssstu />
+      <Description /> */}
+      {/* <LoginPage></LoginPage> */}
+    </div>
+  );
 }
 
 export default App;
