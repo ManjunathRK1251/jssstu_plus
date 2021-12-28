d6f465e41487e54a4a6d1a877940778ebe9fb1ac
Manojkc15 <manojkc9845@gmail.com>
2021-12-28 21:04:47 +0530

    positiones carousel slider

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,20 +2,17 @@ import React from "react";
 import "./Navbar.css";
 import HomePage from "../Homepage/homepage";
 import LoginPage from "../LoginPage/loginpage";
+import CarouselSlider from "../Components/Carousel_Slider/Carousel_slider";
 // import "./background_img.css";
 // import navbar_img from "../Images/navbar_bg.jpg";
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <BrowserRouter>
-
-            <div className="Navbar-bg"></div>
+            <div className="Navbar-bg">
+                <CarouselSlider />
+            </div>
             <div className="navbar">
                 <div className="flex-row">
                     {/* <a href="#About">About</a> */}
@@ -24,16 +21,12 @@ export default function Navbar() {
                     <Link to="/placement">Placement</Link>
                     <Link to="/life-at-jssstu">Life at JSSSTU</Link>
                     <Link to="/faq">FAQ</Link>
-
-
                 </div>
-
             </div>
             {/* <Routes>
                 <Route exact path='/' element={< HomePage />}></Route>
                 <Route exact path='/life-at-jssstu' element={< LoginPage />}></Route>
             </Routes> */}
-
         </BrowserRouter>
 
         // </div>
