3543f94d27bcb0b35c08ff753e39f8b3cf6f9a34
Manojkc15 <manojkc9845@gmail.com>
2022-01-21 23:17:10 +0530

    adds FAQ detail pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import CarouselSlider from "./Components/Carousel_Slider/Carousel_slider";
 import HomePage from "./Homepage/homepage";
 import Navbar from "./Components/Navbar";
 import FAQSection from "./Components/FAQ/Faq";
+import Hostel from "./Components/FAQ/Hostel";
 
 function App() {
     return (
+        // <Hostel />
         <Navbar />
         // <div>
         //     {/* <CarouselSlider /> */}
