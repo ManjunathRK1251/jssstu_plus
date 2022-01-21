16e65b494f7b4352f4f19680d095be01968ba4cd
Gowtham-M1729 <gowtham1842001@gmai.com>
2022-01-21 17:25:53 +0530

    Add faq files

---
+++ src/Components/FAQ/hostel.js
@@ -0,0 +1,19 @@
+var hostel = [
+  {
+    id: 1,
+    "what is the curfew?": "it's 7:30 :",
+  },
+
+  { id: 2, 
+    "how many people in one room?": "4 in off-campus - 3 in on-campus" },
+
+  { id: 3,
+     "what kind of food will we get?": "South Indian and pure veg " },
+  {
+    id: 4,
+    "are we allowed to go home whenever we want?":
+      "yeah, you just have to submit home letter",
+  },
+  { id: 5, "do we get hot water 24x7?": "yes" },
+];
+export default hostel;
