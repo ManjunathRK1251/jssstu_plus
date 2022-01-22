44b7d7ffcb45d39d059dbb7fa87e808eff748db0
Gowtham-M1729 <gowtham1842001@gmai.com>
2022-01-22 10:58:21 +0530

    Add Placement FAQ data

--- src/Components/FAQ/placement.js
+++ src/Components/FAQ/placement.js
@@ -1,26 +1,21 @@
 var general = [
   {
-    id: 1,
     " How many Companies usually visit the campus during Campus Placements?":
       " Number of Companies visiting the campus for final placement each year is around 90. Last year more than 100 companies visited SJCE for final placement.",
   },
   {
-    id: 2,
     "What is the help extended to the students for placements?":
       "The placement team facilitates the students throughout the year with different activities. The students are exposed to experts from the industry who enlighten them and polish their skills according to the industry. The students are provided with company specific workshops and online aptitude tests to sharpen their logical and analytical skills before the placement processes. Mock GD and PI sessions are also held for the students to facilitate them in the preparation for the final interviews.",
   },
   {
-    id: 3,
     "Which are the Companies that regularly visit for Campus Recruitment?":
       "See our Valued Recruiters list.",
   },
   {
-    id: 4,
     "When does the Placement Activity begin?":
       "The final placement season starts from the month of October every year and ends by February. Summer Internship selection process starts in the month of February and ends by mid April.",
   },
   {
-    id: 5,
     "Who takes Decisions on Academic matters?":
       "Yes, one must have a CGPA of more than 7 on average on a scale of 10 to get into the final placement process.But it usually depends on the comapnies visiting",
   },
