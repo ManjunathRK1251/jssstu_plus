ef6cfe8483bc38555ce50e485d372e10cf7c605e
Manojkc15 <manojkc9845@gmail.com>
2022-01-25 00:45:23 +0530

    implemented placement2 page

---
+++ src/Placement/Placement2.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./Placement2.css";
+import placement_2 from "../Images/placement_2.jpg";
+
+export default function Placement2() {
+    return (
+        <div>
+            <img src={placement_2} className="background-placement2" />
+            <div className="Rectangle-35">
+                <h1 className="placement2-h1">Person name</h1>
+                <p className="placement2-p">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut
+                </p>
+            </div>
+        </div>
+    );
+}
